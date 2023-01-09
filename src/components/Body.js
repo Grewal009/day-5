0ae9c13a5412c4f4257b4356cb2a1548ad81f0ca
aneel.grewal <aneel.grewal@hotmail.com>
2023-01-09 21:09:17 +0100

    class solution

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../config";
+import { useState } from "react";
+
+function dataFilter(searchText, restaurants){
+    const filterData = restaurants.filter((r) =>
+    r.data.name.includes(searchText)
+  );
+
+  return filterData;
+    
+}
 
 const Body = () => {
+    
+    const [searchText,setSearchText] = useState("");
+    const [restaurants, setRestaurants] = useState(restaurantList);
+
+
+
     return(
        /* <div className="restaurent-list">
         <RestaurantCard restaurant = {restaurantList[0]} />
@@ -12,20 +28,37 @@ const Body = () => {
         <RestaurantCard restaurant = {restaurantList[5]}/>
         </div>
        */ 
+    <>
+        <div className="search-container">
+            <input type="text" className="search-input" placeholder="Search" value={searchText} onChange={
+                (e)=>{
+                    setSearchText(e.target.value);
+                }
 
-        //using .map() instead hard coded
-        
+            } />
+            
+            <button className="search-btn" onClick={    
+                ()=>{
+                  const data = dataFilter(searchText, restaurants);
+                  setRestaurants(data);
+                  console.log(restaurants);
+                }
+
+            }    >Search</button>
+
+        </div>
+
+        {/*using .map() instead hard coded*/}
        <div className="restaurent-list">
         {
-             restaurantList.map((restaurant)=>{
+             restaurants.map((restaurant)=>{
                 return(
                     <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>
                 );
             })
         }
-
        </div>
-
+    </>
     );
 };
 
