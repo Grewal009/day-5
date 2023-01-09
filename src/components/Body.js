fa69be01b19322d09c0c72577c56a1a1dfc7912d
aneel.grewal <aneel.grewal@hotmail.com>
2023-01-09 11:32:19 +0100

    RestaurantCard.js added to src folder

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,5 @@
+import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../config";
-import { IMG_CDN_URL } from "../config";
-
-
-const RestaurantCard = ({cloudinaryImageId, name, cuisines, avgRating}) => {
-    return(
-        <div className="card">
-            
-            <img src={IMG_CDN_URL+ cloudinaryImageId}/>
-            <h2>{name}</h2>
-            <h3>{cuisines?.join(", ")}</h3>
-            <h4>{avgRating} rating</h4>
-            
-        </div>
-    );
-};
 
 const Body = () => {
     return(
